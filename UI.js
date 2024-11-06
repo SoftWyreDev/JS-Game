1a91ae2beba543cfc094808be2e8d8606a4dcaa7
Alex <bahler831@gmail.com>
2024-11-05 20:29:19 -0800

    Final Polish

--- UI.js
+++ UI.js
@@ -5,10 +5,33 @@ export class UI {
         this.fontFamily = 'Helvetica';
     }
     draw(context){
+        context.save();
+        context.shadowOffsetX = 2;
+        context.shadowOffsetY = 2;
+        context.shadowColor = 'white';
+        context.shadowBlur = 0;
         context.font = this.fontSize + 'px ' + this.fontFamily;
         context.textAlign = 'left';
         context.fillStyle = this.game.fontColor;
         // score
         context.fillText('Score: ' + this.game.score, 20, 50)
+        // timer
+        context.font = this.fontSize * 0.8 + ' px' + this.fontFamily;
+        context.fillText('Time: ' + (this.game.time * 0.001).toFixed(1), 20, 80);
+        // game over messages
+        if (this.game.gameOver){
+            context.textAlign = 'center';
+            context.font = this.fontSize * 2 + 'px ' + this.fontFamily;
+            if (this.game.score > 5) {
+                context.fillText('Good Shit', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
+                context.fillText('You might be a God', this.game.width * 0.5, this.game.height * 0.5 + 20);
+            } else {
+                context.fillText('You suck', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
+                context.fillText('Get good', this.game.width * 0.5, this.game.height * 0.5 + 20);
+            }
+        }
+        context.restore();
     }
 }
